49b0d1dd5ed4f95093fed168d610b6c0ea3baf20
rayaneaitallaoua <ayoubaitallaoua@gmail.com>
2024-11-21 18:04:09 +0100

    create interval chromosome + read
    interval de chromosome
    position start end pour les reads

--- Rayane.py
+++ Rayane.py
@@ -33,11 +33,62 @@ flag_distinct_reps = {}
 for flag in flag_list:
 
     #si le flag existe déja, prends la valeur précédante + 1
-    if flag in flag_distinct_reps :
+    if flag in flag_distinct_reps:
         flag_distinct_reps[flag] = flag_distinct_reps[flag] + 1
     #Sinon tu crée la clé avec le flag et tu l'initialise à 1
-    else :
+    else:
         flag_distinct_reps[flag] = 1
 
-print(flag_distinct_reps)
+
+#print(flag_distinct_reps)
+
+def divise_chromosome(longueur_chrom: int, taille_interv: int):
+    interv_start_dict = {}
+    interv_end_dict = {}
+
+    interv_index = 0
+    interv_start = 1
+
+    while interv_start < longueur_chrom:
+        interv_index += 1
+        interv_name = f"Interval_{interv_index}"
+        interv_end = interv_start + taille_interv + 1
+
+        while interv_end >= longueur_chrom:
+            interv_end = interv_end - 1
+
+        interv_start_dict[interv_name] = interv_start
+        interv_end_dict[interv_name] = interv_end
+
+        interv_start = interv_end + 1
+
+    return interv_start_dict, interv_end_dict
+
+
+start, end = divise_chromosome(1000, 100)
+
+print(start)
+
+def read_interval(file_path: str):
+    dict_read_start = {}
+    dict_read_end = {}
+
+    with open(file_path, 'r') as file:
+        # pour chaque ligne dans mon fichier
+        for line in file:
+            # ignorer les headers
+            if line.startswith('@'):
+                continue
+
+            # Diviser les colonnes en utilisant \t
+            columns = line.split('\t')
+
+            read_name = columns[0]
+            read_start = columns[3]
+            dict_read_start[read_name] = read_start
+
+            read_end = read_start + columns[8]
+            dict_read_end[read_name] = read_end
+
+    return dict_read_start, dict_read_end
 
