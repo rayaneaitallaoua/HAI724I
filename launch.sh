72b51ee0283b028062d1f7553ac7bdf3ae32284f
farahmouk <farahmoukachar@hotmail.com>
2024-12-03 11:04:49 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 file_path = $1
 if grep -q '^@HD' "$file_path"; then
 	echo "the given file is a sam file. Python will be executed"
-	python3 main.py "$file_path"
+	python3 Main.py "$file_path"
 else
 	echo "it is not a sam file"
